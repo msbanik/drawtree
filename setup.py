df583498ca08617617bce80f325dc2f75920b9b0
Madhusudan Banik <msbanik@gmail.com>
2015-10-02 17:11:35 -0700

    update release number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="drawtree",
-    version="0.1.0",
+    version="0.1.1",
     url="https://github.com/msbanik/drawtree.git",
 
     author="Madhusudan Banik",

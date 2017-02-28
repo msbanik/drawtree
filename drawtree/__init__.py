f2a253e4dc7ec3df4dc89d867b5c28f4d1928599
joncrall <crallj@rpi.edu>
2017-02-28 10:35:50 -0500

    Fixed python3 issues

--- drawtree/__init__.py
+++ drawtree/__init__.py
@@ -1,7 +1,8 @@
+# -*- coding: utf-8 -*-
 """drawtree - Draw binary tree in plain text"""
 
 __version__ = '0.1.0'
 __author__ = 'Madhusudan Banik <msbanik@gmail.com>'
 __all__ = []
 
-from drawtree import draw_bst, draw_random_bst, draw_level_order
+from drawtree.drawtree import draw_bst, draw_random_bst, draw_level_order
